85deabc432a6b0653997fa6606f97ed8ddf5ecd7
João Pedro Alves <jotalmeida007@hotmail.com>
2020-08-07 15:51:41 -0300

    update

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import {promises as fs } from "fs";
+let fs = require('fs').promises;
 
 createFiles();
 
   async function createFiles() {  
-    data = await fs.readFile("./json/Estados.json");
+    let data = await fs.readFile("./files/Estados.json");
     const states = JSON.parse(data);
 
-    let data = await fs.readFile("./json/Cidades.json");
+    data = await fs.readFile("./files/Cidades.json");
     const cities = JSON.parse(data);
-      
+
     for (state of states) {
-      const stateCities = cities.filter(city.Estado === state.ID);
-      await fs.writeFile(`./states/${state.Sigla}.json`, JSON.stringify(stateCities));
+       const stateCities = cities.filter( city => city.Estado === state.ID);
+       await fs.writeFile(`./states/${state.Sigla}.json`, JSON.stringify(stateCities));
     }
-console.log(states);
 }

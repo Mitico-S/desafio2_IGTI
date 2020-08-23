0662dc3c5185c88488f1ae527c0c826b26e97d7f
João Pedro Alves <jotalmeida007@hotmail.com>
2020-08-23 11:07:43 -0300

    fix bug await function

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ let fs = require('fs').promises;
 
 createFiles();
 
-  async function createFiles() {  
-    let data = await fs.readFile("./files/Estados.json");
-    const states = JSON.parse(data);
+async function createFiles() {
+  let data = await fs.readFile('./files/Estados.json');
+  const states = JSON.parse(data);
 
-    data = await fs.readFile("./files/Cidades.json");
-    const cities = JSON.parse(data);
+  data = await fs.readFile('./files/Cidades.json');
+  const cities = JSON.parse(data);
 
-    for (state of states) {
-       const stateCities = cities.filter( city => city.Estado === state.ID);
-       await fs.writeFile(`./states/${state.Sigla}.json`, JSON.stringify(stateCities));
-    }
+  for (state of states) {
+    const stateCities = cities.filter((city) => city.Estado === state.ID);
+    await fs.writeFile(
+      `./states/${state.Sigla}.json`,
+      JSON.stringify(stateCities)
+    );
+  }
 }
